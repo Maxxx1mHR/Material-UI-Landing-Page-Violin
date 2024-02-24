1c0f5e1a2eb38a938627808aa1aca79b75a8edd8
Maxim Pudeev <maksim.pudeev@bk.ru>
2024-02-25 00:00:36 +0300

    chore: test server

--- server/server.js
+++ server/server.js
@@ -1,45 +1,41 @@
 import express from "express";
 import bodyParser from "body-parser";
 import nodemailer from "nodemailer";
-import cors from "cors"; // Импортируем пакет cors
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-app.use(cors()); // Добавляем cors middleware
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post("/send-email", async (req, res) => {
   const { name, email, message } = req.body;
 
-  // Настройте транспорт для отправки почты
   const transporter = nodemailer.createTransport({
-    host: "smtp.mail.ru", // SMTP-сервер почтового сервиса bk.ru
-    port: 465, // Порт SMTP-сервера
-    secure: true, // Использовать SSL
+    host: "smtp.mail.ru",
+    port: 465,
+    secure: true,
     auth: {
-      user: process.env.EMAIL_ADDRESS, // Ваш электронный адрес Gmail
-      pass: process.env.EMAIL_PASSWORD, // Пароль от вашего почтового ящика
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
 
-  // Настройте содержимое письма
   const mailOptions = {
     from: process.env.EMAIL_ADDRESS,
-    to: process.env.EMAIL_ADDRESS, // Получатель
-    subject: "New Message from Contact Form", // Тема письма
+    to: process.env.EMAIL_ADDRESS,
+    subject: "New Message from Contact Form",
     text: `
       Name: ${name}
       Email: ${email}
       Message: ${message}
-    `, // Текст письма
+    `,
   };
 
   try {
-    // Отправляем письмо
     await transporter.sendMail(mailOptions);
     console.log("Email sent successfully");
     res.status(200).send("Email Sent Successfully");
@@ -49,6 +45,4 @@ app.post("/send-email", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+export default app;
